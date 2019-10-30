494ca1a29f0f6549a404133626c87df0aaf59de9
Bent Tranberg <bent.tranberg@gmail.com>
2019-10-30 21:13:26 +0100

    Copied stuff from ExploreElmishWpf, just for starters.

---
+++ ExploreElmishWpfDx.Models/CounterPane.fs
@@ -0,0 +1,75 @@
+﻿namespace ExploreElmishWpfDx.Models
+
+module CounterPane =
+
+    open System
+    open System.Windows
+    open Elmish
+    open Elmish.WPF
+
+    type Model =
+        {
+            Count: int
+            StepSize: int
+        }
+
+    let private initialModel =
+        {
+            Count = 0
+            StepSize = 1
+        }
+
+    let init () =
+        initialModel, Cmd.none
+
+    type Msg =
+        | Increment
+        | Decrement
+        | SetStepSize of int
+        | Reset
+        | Close
+
+    let update msg m =
+        match msg with
+        | Increment -> { m with Count = m.Count + m.StepSize }, Cmd.none
+        | Decrement -> { m with Count = m.Count - m.StepSize }, Cmd.none
+        | SetStepSize x -> { m with StepSize = x }, Cmd.none
+        | Reset -> init ()
+        | Close -> m, Cmd.none  // handled by parent
+
+    let bindings () : Binding<Model, Msg> list =
+        [
+            "Count" |> Binding.oneWay (fun m -> m.Count)
+            "Increment" |> Binding.cmd Increment
+            "Decrement" |> Binding.cmd Decrement
+            "StepSize" |> Binding.twoWay ((fun m -> float m.StepSize), int >> SetStepSize)
+            "Reset" |> Binding.cmdIf (Reset, (<>) initialModel)
+            "Close" |> Binding.cmd Close
+        ]
+
+    // This is the design time model actually used for CounterPane, and it's the only place in the solution where
+    // this way of doing it is demonstrated.
+    // Unfortunately it does not work reliably in .NET Core 3.0 at this time. You will frequently not see the
+    // design time data.
+    // If you change the framework target to one of .NET Framework, then it will work reliably.
+    // If you type a letter in front of the "{" after "DataContext", and then remove it, refresh is triggered.
+    // However, the data won't display properly for long, so you have to repeat doing this "manual" refresh.
+    // The corresponding XAML :
+    // d:DataContext="{x:Static vm:CounterPane.designTimeModel}"
+    let dtModel = { Count = 3; StepSize = 7 }
+    let dtBindings = bindings ()
+    let designTimeModel = ViewModel.designInstance dtModel dtBindings
+
+// An alternative design time model that does not have the problem.
+// This way of doing it works reliably also in .NET Core 3.0, and is the choice elsewhere in this solution until
+// the problem is fixed. The drawback is that the runtime model type can't be used - but that turns out not to
+// be that much of a drawback. It's usually simpler and quicker, and the fact that the two models might get out
+// of sync turns out not to be a big deal. And although the runtime model can't be used directly, often the
+// surrounding functionality is quite helpful.
+// The reason for this choice is that I will be developing for .NET Core 3+ in the future, and not .NET Framework,
+// so I want to go with the solution that works reliably for .NET Core now, to avoid frustrations while designing.
+// The corresponding XAML :
+// d:DataContext="{d:DesignInstance vm:CounterPaneDesignTimeModel, IsDesignTimeCreatable=True}"
+type CounterPaneDesignTimeModel() =
+    member val Count = 4 with get, set
+    member val StepSize = 8 with get, set
--- ExploreElmishWpfDx.Models/MainWindow.fs
+++ ExploreElmishWpfDx.Models/MainWindow.fs
@@ -7,27 +7,103 @@ module MainWindow =
     open Elmish
     open Elmish.WPF
 
-    type Message =
-        | ButtonClick
+    type Pane =
+        | Form1 of Form1.Model
+        | Form2 of Form2.Model
+        | CounterPane of CounterPane.Model
+        | TabsPane of TabsPane.Model
 
     type Model =
         {
-            LabelText: string
+            Pane: Pane option
         }
 
     let init () =
         {
-            LabelText = "Anybody here?"
+            Pane = None
         }, Cmd.none
 
-    let update message model =
-        match message with
-        | ButtonClick -> { model with LabelText = "Oh, hello!" }, Cmd.none
+    type Msg =
+        | ShowForm1
+        | ShowForm2
+        | ShowCounterPane
+        | ShowTabsPane
+        | Form1Msg of Form1.Msg
+        | Form2Msg of Form2.Msg
+        | CounterPaneMsg of CounterPane.Msg
+        | TabsPaneMsg of TabsPane.Msg
 
-    let bindings (model: Model) dispatch : Binding<Model, Message> list =
+    let update msg m =
+        match msg with
+        | ShowForm1 -> { m with Pane = Some <| Form1 Form1.init }, Cmd.none
+        | ShowForm2 -> { m with Pane = Some <| Form2 Form2.init }, Cmd.none
+        | ShowCounterPane ->
+            let m', cmd' = CounterPane.init ()
+            { m with Pane = Some <| CounterPane m' }, cmd'
+        | ShowTabsPane ->
+            let m', cmd' = TabsPane.init ()
+            { m with Pane = Some <| TabsPane m' }, cmd'
+        | Form1Msg Form1.Submit -> { m with Pane = None }, Cmd.none
+        | Form1Msg msg' ->
+            match m.Pane with
+            | Some (Form1 m') -> { m with Pane = Form1.update msg' m' |> Form1 |> Some }, Cmd.none
+            | _ -> m, Cmd.none
+        | Form2Msg Form2.Submit -> { m with Pane = None }, Cmd.none
+        | Form2Msg msg' ->
+            match m.Pane with
+            | Some (Form2 m') -> { m with Pane = Form2.update msg' m' |> Form2 |> Some }, Cmd.none
+            | _ -> m, Cmd.none
+        | CounterPaneMsg CounterPane.Close -> { m with Pane = None }, Cmd.none
+        | CounterPaneMsg counterPaneMsg ->
+            match m.Pane with
+            | Some (CounterPane m') ->
+                let pane, paneCmd = CounterPane.update counterPaneMsg m'
+                { m with Pane = pane |> CounterPane |> Some }, paneCmd
+            | _ -> m, Cmd.none
+        | TabsPaneMsg TabsPane.Close -> { m with Pane = None }, Cmd.none
+        | TabsPaneMsg tabsPaneMsg ->
+            match m.Pane with
+            | Some (TabsPane m') ->
+                let pane, paneCmd = TabsPane.update tabsPaneMsg m'
+                { m with Pane = pane |> TabsPane |> Some }, paneCmd
+            | _ -> m, Cmd.none
+
+    let bindings (model: Model) dispatch : Binding<Model, Msg> list =
         [
-            "ButtonClick" |> Binding.cmd (fun _ -> ButtonClick)
-            "LabelContent" |> Binding.oneWay (fun m -> m.LabelText)
+            "ShowForm1" |> Binding.cmd ShowForm1
+            "ShowForm2" |> Binding.cmd ShowForm2
+            "ShowCounterPane" |> Binding.cmd ShowCounterPane
+            "ShowTabsPane" |> Binding.cmd ShowTabsPane
+            "PaneVisible" |> Binding.oneWay (fun m -> m.Pane.IsSome)
+            "NotPaneVisible" |> Binding.oneWay (fun m -> m.Pane.IsNone)
+            "Form1Visible" |> Binding.oneWay
+                (fun m -> match m.Pane with Some (Form1 _) -> true | _ -> false)
+            "Form2Visible" |> Binding.oneWay
+                (fun m -> match m.Pane with Some (Form2 _) -> true | _ -> false)
+            "CounterPaneVisible" |> Binding.oneWay
+                (fun m -> match m.Pane with Some (CounterPane _) -> true | _ -> false)
+            "TabsPaneVisible" |> Binding.oneWay
+                (fun m -> match m.Pane with Some (TabsPane _) -> true | _ -> false)
+            "Form1" |> Binding.subModelOpt (
+                (fun m -> match m.Pane with Some (Form1 m') -> Some m' | _ -> None),
+                snd,
+                Form1Msg,
+                Form1.bindings)
+            "Form2" |> Binding.subModelOpt (
+                (fun m -> match m.Pane with Some (Form2 m') -> Some m' | _ -> None),
+                snd,
+                Form2Msg,
+                Form2.bindings)
+            "CounterPane" |> Binding.subModelOpt (
+                (fun m -> match m.Pane with Some (CounterPane m') -> Some m' | _ -> None),
+                snd,
+                CounterPaneMsg,
+                CounterPane.bindings)
+            "TabsPane" |> Binding.subModelOpt (
+                (fun m -> match m.Pane with Some (TabsPane m') -> Some m' | _ -> None),
+                snd,
+                TabsPaneMsg,
+                TabsPane.bindings)
         ]
 
     let entryPoint (_: string[], mainWindow: Window) =
